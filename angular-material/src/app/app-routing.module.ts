0a2c1084da344f05eba2985e76620f66ff39b8d6
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2019-03-03 18:21:56 +0530

    routing had been included

--- angular-material/src/app/app-routing.module.ts
+++ angular-material/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicFormComponent } from './basic-form/basic-form.component';
+import { LoginComponent } from './login/login.component';
+import { BasicDataComponent } from './basic-data/basic-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'login', component: LoginComponent },
+	{ path: 'data', component: BasicDataComponent },
+	{ path: '**', component: BasicFormComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -8,12 +8,16 @@ import { BasicFormComponent } from './basic-form/basic-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { BasicDataComponent } from './basic-data/basic-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicFormComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    BasicDataComponent
   ],
   imports: [
     BrowserModule,
