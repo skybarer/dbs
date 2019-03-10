96f42dd8f23394a1c9ad63b4b126ab0daf0f7882
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2019-03-10 12:08:09 +0530

    added  icon state to the existing list.

--- ng-alpha/src/app/components/stepper/stepper.component.ts
+++ ng-alpha/src/app/components/stepper/stepper.component.ts
@@ -15,27 +15,59 @@ export class StepperComponent implements OnInit {
       'link': 'application-contact-info',
       'subItems': [
         { 'name': 'Application Contact Info', 'link': 'application-contact-info' },
-        { 'name': 'Select Account type', 'link': 'select-account-type' }]
+        { 'name': 'Select Account type', 'link': 'select-account-type' }
+      ]
     },
-    { 'name': 'Enterprise Information', 'link': 'enterprise-information'  },
-    { 'name': 'Bank of service application', 'link': 'bank-of-service-application'  },
+    {
+      'name': 'Enterprise Information',
+      'link': 'enterprise-information',
+      'subItems': [
+        { 'name': 'Enterprise Information', 'link': 'enterprise-information' },
+        { 'name': 'Select Account type', 'link': 'select-account-type' }
+      ]
+     },
+    { 'name': 'Bank of service application', 'link': 'bank-of-service-application' },
     { 'name': 'File Uplaod', 'link': 'file-uplaod' },
     { 'name': 'Confirm', 'link': 'confirm' }
   ];
 
   itemsLength: number;
-
+  subItemsLength: number;
+  selectedUserIndex: number;
+  // iconStateMachine = [];
 
 
   ngOnInit() {
     this.itemsLength = this.items.length;
+    for (let i = 0; i < this.itemsLength; i++) {
+      (this.items[i])['tickIcon'] = false;
+      (this.items[i])['arrowIcon'] = false;
+      if (this.items[i].subItems) {
+        for (let j = 0; j < this.items[i].subItems.length; j++) {
+          (this.items[i].subItems[j])['tickIcon'] = false;
+          (this.items[i].subItems[j])['arrowIcon'] = false;
+        }
+      }
+    }
+    (this.items[0])['tickIcon'] = true;
+    (this.items[0].subItems[0])['tickIcon'] = true;
+    console.log(this.items);
   }
 
-  // to remove last divider in list
-  isMdDividerRequired = (index: number) => {
+  // to remove last divider in item-list
+  isMdDividerRequiredForListItem = (index: number) => {
     if (index === this.itemsLength - 1) { return false; }
     return true;
   }
+
+  // show and hide functionality of sublist items based on onclick.
+    selectUserIndex =  (index: any) => {
+      if (this.selectedUserIndex !== index) {
+      this.selectedUserIndex = index;
+      } else {
+        this.selectedUserIndex = undefined;
+      }
+   }
 
   previousLink = () => {
 
